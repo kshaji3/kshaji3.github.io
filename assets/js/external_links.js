c141139656caabd65229a9c5e9b80d68ff90d9df
Karthik Shaji <karthikshaji@machindustries.com>
2026-03-11 12:11:32 -0400

    only page left is projects

---
+++ assets/js/external_links.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const links = document.querySelectorAll("a[href]");
+
+  links.forEach(link => {
+    const url = new URL(link.href, window.location.href);
+
+    if (url.hostname !== window.location.hostname) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+    }
+
+  });
+
+});
